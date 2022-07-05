a534e4bcf49e8d9f12470db8f50f074c1f4a4e4a
riku <riku.suny@gmail.com>
2022-07-05 22:20:28 +0900

    add react-navigation

--- App.tsx
+++ App.tsx
@@ -1,49 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
+import { Root } from './src/Root';
 
 export default function App() {
   return (
-    <SafeAreaView style={{ flex: 1, marginHorizontal: 16 }}>
-      <Text
-        style={{
-          fontWeight: 'bold',
-          fontSize: 32,
-          marginTop: 4,
-          fontFamily: 'Chalkboard SE',
-          color: '#0f1d61',
-        }}
-      >
-        Home
-      </Text>
-      <Text
-        style={{
-          fontSize: 16,
-          fontWeight: 'bold',
-          marginTop: 20,
-          color: '#262626',
-        }}
-      >
-        This is テキスト
-      </Text>
-      <View
-        style={{
-          width: '100%',
-          height: 60,
-          backgroundColor: '#526eff',
-          borderRadius: 6,
-          position: 'absolute',
-          bottom: 64,
-          alignSelf: 'center',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <Text style={{ fontWeight: 'bold', color: 'white', fontSize: 18 }}>
-          送信
-        </Text>
-      </View>
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Root />
+    </NavigationContainer>
   );
 }

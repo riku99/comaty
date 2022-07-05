8e1e582c3e7ed026f16944d62d8056097d98565b
riku <riku.suny@gmail.com>
2022-07-05 21:43:34 +0900

    add styles

--- App.tsx
+++ App.tsx
@@ -1,21 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hi</Text>
+    <SafeAreaView style={{ flex: 1, marginHorizontal: 16 }}>
+      <Text
+        style={{
+          fontWeight: 'bold',
+          fontSize: 32,
+          marginTop: 4,
+          fontFamily: 'Chalkboard SE',
+          color: '#0f1d61',
+        }}
+      >
+        Home
+      </Text>
+      <Text
+        style={{
+          fontSize: 16,
+          fontWeight: 'bold',
+          marginTop: 20,
+          // fontFamily: 'ArialMT',
+        }}
+      >
+        This is テキスト
+      </Text>
+      <View
+        style={{
+          width: '100%',
+          height: 60,
+          backgroundColor: '#526eff',
+          borderRadius: 6,
+          position: 'absolute',
+          bottom: 64,
+          alignSelf: 'center',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Text style={{ fontWeight: 'bold', color: 'white', fontSize: 18 }}>
+          送信
+        </Text>
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ src/styles.ts
@@ -0,0 +1,6 @@
+const colors = {
+  main: '#526eff',
+  headerTitleFont: 'Chalkboard SE',
+  headerTitleColor: '#0f1d61',
+  headerTitleSize: 32,
+};

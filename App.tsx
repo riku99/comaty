478f0ff328ffd705ff0cbbbd4acc6dd28abefc6d
riku <riku.suny@gmail.com>
2022-07-05 22:36:10 +0900

    absolute import

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import { Root } from './src/Root';
+import { Root } from 'src/Root';
 
 export default function App() {
   return (

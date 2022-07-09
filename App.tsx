a7853c5281b202db6fa9e37b20ef1c9271fd3c9a
riku <riku.suny@gmail.com>
2022-07-09 16:42:34 +0900

    fix pods

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Config from 'react-native-config';
 import { ApolloProvider } from 'src/providers/ApolloProvider';
 import { NavigationProvider } from 'src/providers/NavigationProvider';
 import { ThemeProvider } from 'src/providers/ThemeProvider';
 import { Root } from 'src/Root';
 
 export default function App() {
+  console.log('🌟 ENV is ' + Config.ENV);
   return (
     <ApolloProvider>
       <ThemeProvider>

a23fb85deace36018b0301cf4d4c1f8e92f2ab4a
riku <riku.suny@gmail.com>
2022-07-10 14:14:11 +0900

    fix query

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,20 +1,20 @@
 import React, { useEffect } from 'react';
 import { LoadingOverlay } from 'src/components/LoadingOverlay';
-import { useGetMeQuery } from 'src/generated/graphql';
+import { useGetInitialDataQuery } from 'src/generated/graphql';
 import { useLoggedIn } from 'src/hooks/auth';
 import { useLoadingVisible } from 'src/hooks/loadingOverlay';
 import { RootStack } from 'src/navigations/RootStack';
 
 export const Root = () => {
   const { loadingVisible } = useLoadingVisible();
-  const { data: meData } = useGetMeQuery();
+  const { data: initialData } = useGetInitialDataQuery();
   const { setLoggedIn } = useLoggedIn();
 
   useEffect(() => {
-    if (meData?.me) {
+    if (initialData?.me) {
       setLoggedIn(true);
     }
-  }, [meData]);
+  }, [initialData]);
 
   return (
     <>

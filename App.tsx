e475b43545353ec24c81137e94e1a9c335e8a86d
riku <riku.suny@gmail.com>
2022-07-12 20:45:49 +0900

    setup gesture handler

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
 import Config from 'react-native-config';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { ApolloProvider } from 'src/providers/ApolloProvider';
 import { NavigationProvider } from 'src/providers/NavigationProvider';
 import { ThemeProvider } from 'src/providers/ThemeProvider';
@@ -8,12 +10,20 @@ import { Root } from 'src/Root';
 export default function App() {
   console.log('🌟 ENV is ' + Config.ENV);
   return (
-    <ApolloProvider>
-      <ThemeProvider>
-        <NavigationProvider>
-          <Root />
-        </NavigationProvider>
-      </ThemeProvider>
-    </ApolloProvider>
+    <GestureHandlerRootView style={styles.gestureHandlerRootView}>
+      <ApolloProvider>
+        <ThemeProvider>
+          <NavigationProvider>
+            <Root />
+          </NavigationProvider>
+        </ThemeProvider>
+      </ApolloProvider>
+    </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  gestureHandlerRootView: {
+    flex: 1,
+  },
+});

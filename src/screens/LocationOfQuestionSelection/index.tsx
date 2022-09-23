b8629ffda26ce1734006b5f76a5cf05c1f0b3e59
riku <riku.suny@gmail.com>
2022-09-23 15:06:43 +0900

    add locatio input

--- src/screens/LocationOfQuestionSelection/index.tsx
+++ src/screens/LocationOfQuestionSelection/index.tsx
@@ -1,6 +1,13 @@
 import { Button, Text } from '@rneui/themed';
+import debounce from 'lodash.debounce';
 import { useLayoutEffect, useState } from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, View } from 'react-native';
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  TextInput,
+  View,
+} from 'react-native';
 import Geocoder from 'react-native-geocoding';
 import MapView, { MapPressEvent, Marker } from 'react-native-maps';
 import { RadioButton } from 'src/components/ui/RadioButton';
@@ -41,6 +48,22 @@ export const LocationOfQuestionSelectionScreen = ({ navigation }: Props) => {
     }
   };
 
+  const onChangeSearchText = async (text: string) => {
+    console.log(text);
+    if (!text) {
+      return;
+    }
+
+    try {
+      const resultJson = await Geocoder.from(text);
+      resultJson.results.forEach((result) => {
+        console.log(result);
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <SafeAreaView
@@ -48,7 +71,7 @@ export const LocationOfQuestionSelectionScreen = ({ navigation }: Props) => {
           flex: 1,
         }}
       >
-        <ScrollView>
+        <ScrollView contentContainerStyle={styles.scrollContainer}>
           <MapView
             style={styles.map}
             initialRegion={{
@@ -61,6 +84,36 @@ export const LocationOfQuestionSelectionScreen = ({ navigation }: Props) => {
           >
             {selectedCoodinate && <Marker coordinate={selectedCoodinate} />}
           </MapView>
+
+          <View
+            style={{
+              position: 'absolute',
+              top: 10,
+              width: '96%',
+              alignSelf: 'center',
+            }}
+          >
+            <TextInput
+              placeholder="マップで検索"
+              onChangeText={debounce((text) => {
+                onChangeSearchText(text);
+              }, 1000)}
+              style={{
+                backgroundColor: '#fff',
+                height: 44,
+                borderRadius: 22,
+                paddingHorizontal: 14,
+                fontSize: 16,
+                shadowColor: '#000',
+                shadowOffset: {
+                  width: 3,
+                  height: 8,
+                },
+                shadowOpacity: 0.24,
+                shadowRadius: 22,
+              }}
+            />
+          </View>
 
           <View style={styles.withoutMapContents}>
             <Text style={[styles.askTitle, { marginTop: 14 }]}>
@@ -127,8 +180,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  scrollContainer: {
+    paddingBottom: 54,
+  },
   map: {
-    height: 250,
+    height: 340,
     width: '100%',
   },
   withoutMapContents: {

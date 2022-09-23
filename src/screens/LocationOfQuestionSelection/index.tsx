8f3719d582b5ad5598f4819cd5559cf59539bb4d
riku <riku.suny@gmail.com>
2022-09-23 14:03:32 +0900

    add button

--- src/screens/LocationOfQuestionSelection/index.tsx
+++ src/screens/LocationOfQuestionSelection/index.tsx
@@ -1,6 +1,6 @@
-import { Text } from '@rneui/themed';
+import { Button, Text } from '@rneui/themed';
 import { useLayoutEffect, useState } from 'react';
-import { ScrollView, StyleSheet, View } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, View } from 'react-native';
 import Geocoder from 'react-native-geocoding';
 import MapView, { MapPressEvent, Marker } from 'react-native-maps';
 import { RadioButton } from 'src/components/ui/RadioButton';
@@ -43,67 +43,82 @@ export const LocationOfQuestionSelectionScreen = ({ navigation }: Props) => {
 
   return (
     <View style={styles.container}>
-      <ScrollView>
-        <MapView
-          style={styles.map}
-          initialRegion={{
-            latitude: 35.691272864352946,
-            longitude: 140.02043343209206,
-            latitudeDelta: 0.005,
-            longitudeDelta: 0.005,
-          }}
-          onPress={onMapPress}
-        >
-          {selectedCoodinate && <Marker coordinate={selectedCoodinate} />}
-        </MapView>
-
-        <View style={styles.withoutMapContents}>
-          <Text style={[styles.askTitle, { marginTop: 14 }]}>
-            どの辺にいる人に質問する？
-          </Text>
-          <Text style={styles.selectedAddress}>
-            {selectedLocationAddress
-              ? selectedLocationAddress
-              : 'マップから選択してください🙃'}
-          </Text>
-
-          <Text style={[styles.askTitle, { marginTop: 40 }]}>
-            そこからどれくらいの範囲にいるユーザーに質問する？
-          </Text>
-
-          <VStack
-            space={20}
-            style={{
-              marginTop: 12,
+      <SafeAreaView
+        style={{
+          flex: 1,
+        }}
+      >
+        <ScrollView>
+          <MapView
+            style={styles.map}
+            initialRegion={{
+              latitude: 35.691272864352946,
+              longitude: 140.02043343209206,
+              latitudeDelta: 0.005,
+              longitudeDelta: 0.005,
             }}
+            onPress={onMapPress}
           >
-            <RadioButton
-              size={22}
-              isSelected={displayRange === ApproximateRange.Wide}
-              label="広め"
-              onPress={() => {
-                setDisplayRange(ApproximateRange.Wide);
+            {selectedCoodinate && <Marker coordinate={selectedCoodinate} />}
+          </MapView>
+
+          <View style={styles.withoutMapContents}>
+            <Text style={[styles.askTitle, { marginTop: 14 }]}>
+              どの辺にいる人に質問する？
+            </Text>
+            <Text style={styles.selectedAddress}>
+              {selectedLocationAddress
+                ? selectedLocationAddress
+                : 'マップから選択してください🙃'}
+            </Text>
+
+            <Text style={[styles.askTitle, { marginTop: 40 }]}>
+              そこからどれくらいの範囲にいるユーザーに質問する？
+            </Text>
+
+            <VStack
+              space={20}
+              style={{
+                marginTop: 12,
               }}
-            />
-            <RadioButton
-              size={22}
-              isSelected={displayRange === ApproximateRange.Normal}
-              label="普通"
-              onPress={() => {
-                setDisplayRange(ApproximateRange.Normal);
-              }}
-            />
-            <RadioButton
-              size={22}
-              isSelected={displayRange === ApproximateRange.Near}
-              label="すぐ近く"
-              onPress={() => {
-                setDisplayRange(ApproximateRange.Near);
-              }}
-            />
-          </VStack>
-        </View>
-      </ScrollView>
+            >
+              <RadioButton
+                size={22}
+                isSelected={displayRange === ApproximateRange.Wide}
+                label="広め"
+                onPress={() => {
+                  setDisplayRange(ApproximateRange.Wide);
+                }}
+              />
+              <RadioButton
+                size={22}
+                isSelected={displayRange === ApproximateRange.Normal}
+                label="普通"
+                onPress={() => {
+                  setDisplayRange(ApproximateRange.Normal);
+                }}
+              />
+              <RadioButton
+                size={22}
+                isSelected={displayRange === ApproximateRange.Near}
+                label="すぐ近く"
+                onPress={() => {
+                  setDisplayRange(ApproximateRange.Near);
+                }}
+              />
+            </VStack>
+          </View>
+        </ScrollView>
+
+        <Button
+          disabled={!selectedCoodinate}
+          containerStyle={{
+            paddingHorizontal: 16,
+          }}
+        >
+          質問を作成する
+        </Button>
+      </SafeAreaView>
     </View>
   );
 };

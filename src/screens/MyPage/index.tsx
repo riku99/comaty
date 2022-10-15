5ba92b56756bccc3f6b14d03d82983d3fd7cef29
riku <riku.suny@gmail.com>
2022-10-16 08:49:47 +0900

    fix action button size

--- src/screens/MyPage/index.tsx
+++ src/screens/MyPage/index.tsx
@@ -62,64 +62,94 @@ export const MyPageScreen = ({ navigation }: Props) => {
 
       <View style={styles.actionButtons1}>
         <ActionButton
-          icon={<MaterialIcons name="settings" size={24} color="#666666" />}
+          icon={
+            <MaterialIcons
+              name="settings"
+              size={ACTION_BUTTON_ICON_SIZE}
+              color={ACTION_BUTTON_COLOR}
+            />
+          }
           title="設定"
           onPress={() => {
             navigation.navigate('EditProfileStack');
           }}
-          buttonSize={60}
+          buttonSize={ACTION_BUTTON_SIZE}
         />
 
         <ActionButton
-          icon={<MaterialIcons name="edit" size={24} color="#666666" />}
+          icon={
+            <MaterialIcons
+              name="edit"
+              size={ACTION_BUTTON_ICON_SIZE}
+              color={ACTION_BUTTON_COLOR}
+            />
+          }
           title="プロフィール編集"
           onPress={() => {
             navigation.navigate('EditProfileStack');
           }}
-          buttonSize={60}
+          buttonSize={ACTION_BUTTON_SIZE}
         />
+      </View>
 
-        {/* <ActionButton
-          icon={
-            <MaterialIcons name="notifications" size={24} color="#666666" />
-          }
-          title="通知"
-          onPress={() => {
-            navigation.navigate('EditProfileStack');
+      <View
+        style={[
+          styles.actionButtons1,
+          {
+            marginTop: 32,
+          },
+        ]}
+      >
+        <View
+          style={{
+            transform: [{ translateX: 2 }],
           }}
-          buttonSize={60}
-        /> */}
+        >
+          <ActionButton
+            icon={
+              <MaterialCommunityIcons
+                name="card-text"
+                size={ACTION_BUTTON_ICON_SIZE}
+                color={ACTION_BUTTON_COLOR}
+              />
+            }
+            title="マイ投稿"
+            onPress={() => {
+              navigation.navigate('MyPosts');
+            }}
+            buttonSize={ACTION_BUTTON_SIZE}
+          />
+        </View>
 
-        <ActionButton
-          icon={
-            <MaterialCommunityIcons
-              name="card-text"
-              size={24}
-              color={'#666666'}
-            />
-          }
-          title="マイ投稿"
-          onPress={() => {
-            navigation.navigate('MyPosts');
+        <View
+          style={{
+            transform: [{ translateX: -18 }],
           }}
-          buttonSize={60}
-        />
-
-        <ActionButton
-          icon={<MaterialIcons name="group" size={24} color={'#666666'} />}
-          buttonSize={60}
-          onPress={() => {
-            navigation.navigate('MyGroup');
-          }}
-          title="グループ"
-        />
+        >
+          <ActionButton
+            icon={
+              <MaterialIcons
+                name="group"
+                size={ACTION_BUTTON_ICON_SIZE}
+                color={ACTION_BUTTON_COLOR}
+              />
+            }
+            buttonSize={ACTION_BUTTON_SIZE}
+            onPress={() => {
+              navigation.navigate('MyGroup');
+            }}
+            title="グループ"
+          />
+        </View>
       </View>
     </View>
   );
 };
 
-const PROFILE_IMAGE_SIZE = 110;
-const ACTION_BUTTON_SIZE = 66;
+const PROFILE_IMAGE_SIZE = 80;
+const ACTION_BUTTON_SIZE = 60;
+const ACTION_BUTTON_COLOR = '#666666';
+const ACTION_BUTTON_ICON_SIZE = 24;
 
 const styles = StyleSheet.create({
   container: {
@@ -136,14 +166,14 @@ const styles = StyleSheet.create({
     marginTop: 12,
   },
   name: {
-    fontSize: 22,
+    fontSize: 20,
     fontWeight: 'bold',
   },
   age: {
     fontSize: 18,
   },
   actionButtons1: {
-    width: '80%',
+    width: '60%',
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',

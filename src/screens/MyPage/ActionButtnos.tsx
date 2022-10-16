4c00fbad0b8ae4f4b3199b8a9cb992b30cea16d9
riku <riku.suny@gmail.com>
2022-10-16 09:19:12 +0900

    fix action buttons

---
+++ src/screens/MyPage/ActionButtnos.tsx
@@ -0,0 +1,76 @@
+import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { ActionButton } from './ActionButton';
+
+export const ActionButtons = () => {
+  const navigation = useNavigation<RootNavigationProp<'MyPage'>>();
+
+  return (
+    <View style={styles.actionButtons1}>
+      <ActionButton
+        icon={
+          <MaterialCommunityIcons
+            name="card-text"
+            size={ACTION_BUTTON_ICON_SIZE}
+            color={ACTION_BUTTON_COLOR}
+          />
+        }
+        title="マイ投稿"
+        onPress={() => {
+          navigation.navigate('MyPosts');
+        }}
+        buttonSize={ACTION_BUTTON_SIZE}
+      />
+      <View style={styles.editButton}>
+        <ActionButton
+          icon={
+            <MaterialIcons
+              name="edit"
+              size={ACTION_BUTTON_ICON_SIZE}
+              color={ACTION_BUTTON_COLOR}
+            />
+          }
+          title="プロフィール編集"
+          onPress={() => {
+            navigation.navigate('EditProfileStack');
+          }}
+          buttonSize={ACTION_BUTTON_SIZE}
+        />
+      </View>
+
+      <ActionButton
+        icon={
+          <MaterialIcons
+            name="group"
+            size={ACTION_BUTTON_ICON_SIZE}
+            color={ACTION_BUTTON_COLOR}
+          />
+        }
+        buttonSize={ACTION_BUTTON_SIZE}
+        onPress={() => {
+          navigation.navigate('MyGroup');
+        }}
+        title="グループ"
+      />
+    </View>
+  );
+};
+
+const ACTION_BUTTON_SIZE = 60;
+const ACTION_BUTTON_COLOR = '#666666';
+const ACTION_BUTTON_ICON_SIZE = 24;
+
+const styles = StyleSheet.create({
+  actionButtons1: {
+    width: '80%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: 22,
+  },
+  editButton: {
+    transform: [{ translateY: 18 }],
+  },
+});
--- src/screens/MyPage/index.tsx
+++ src/screens/MyPage/index.tsx
@@ -1,8 +1,7 @@
-import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
 import { Text } from '@rneui/themed';
 import { filter } from 'graphql-anywhere';
 import React, { useLayoutEffect } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { ScrollView, StyleSheet, View } from 'react-native';
 import { StoryUserCircle } from 'src/components/domain/user/StoryUserCircle';
 import { Loading } from 'src/components/ui/Loading';
 import {
@@ -10,7 +9,7 @@ import {
   StoryUserCircleFragmentDoc,
   useMyPageScreenDataQuery,
 } from 'src/generated/graphql';
-import { ActionButton } from './ActionButton';
+import { ActionButtons } from './ActionButtnos';
 
 type Props = RootNavigationScreenProp<'MyPageMain'>;
 
@@ -32,128 +31,50 @@ export const MyPageScreen = ({ navigation }: Props) => {
 
   return (
     <View style={styles.container}>
-      <StoryUserCircle
-        imageSize={PROFILE_IMAGE_SIZE}
-        storyUserData={filter<StoryUserCircleFragment>(
-          StoryUserCircleFragmentDoc,
-          data.me
-        )}
-        onPress={() => {
-          if (!me?.id) {
-            return;
-          }
-
-          navigation.navigate('Stories', {
-            startingIndex: 0,
-            storyUsers: [
-              {
-                userId: me.id,
-              },
-            ],
-          });
-        }}
-      />
-
-      <View style={styles.nameContainer}>
-        <Text style={styles.name}>
-          {me.nickname}, <Text style={styles.age}>{me.age}</Text>
-        </Text>
-      </View>
-
-      <View style={styles.actionButtons1}>
-        <ActionButton
-          icon={
-            <MaterialIcons
-              name="settings"
-              size={ACTION_BUTTON_ICON_SIZE}
-              color={ACTION_BUTTON_COLOR}
-            />
-          }
-          title="設定"
+      <ScrollView contentContainerStyle={styles.contentContainer}>
+        <StoryUserCircle
+          imageSize={PROFILE_IMAGE_SIZE}
+          storyUserData={filter<StoryUserCircleFragment>(
+            StoryUserCircleFragmentDoc,
+            data.me
+          )}
           onPress={() => {
-            navigation.navigate('EditProfileStack');
+            if (!me?.id) {
+              return;
+            }
+
+            navigation.navigate('Stories', {
+              startingIndex: 0,
+              storyUsers: [
+                {
+                  userId: me.id,
+                },
+              ],
+            });
           }}
-          buttonSize={ACTION_BUTTON_SIZE}
         />
 
-        <ActionButton
-          icon={
-            <MaterialIcons
-              name="edit"
-              size={ACTION_BUTTON_ICON_SIZE}
-              color={ACTION_BUTTON_COLOR}
-            />
-          }
-          title="プロフィール編集"
-          onPress={() => {
-            navigation.navigate('EditProfileStack');
-          }}
-          buttonSize={ACTION_BUTTON_SIZE}
-        />
-      </View>
-
-      <View
-        style={[
-          styles.actionButtons1,
-          {
-            marginTop: 32,
-          },
-        ]}
-      >
-        <View
-          style={{
-            transform: [{ translateX: 2 }],
-          }}
-        >
-          <ActionButton
-            icon={
-              <MaterialCommunityIcons
-                name="card-text"
-                size={ACTION_BUTTON_ICON_SIZE}
-                color={ACTION_BUTTON_COLOR}
-              />
-            }
-            title="マイ投稿"
-            onPress={() => {
-              navigation.navigate('MyPosts');
-            }}
-            buttonSize={ACTION_BUTTON_SIZE}
-          />
+        <View style={styles.nameContainer}>
+          <Text style={styles.name}>
+            {me.nickname}, <Text style={styles.age}>{me.age}</Text>
+          </Text>
         </View>
 
-        <View
-          style={{
-            transform: [{ translateX: -18 }],
-          }}
-        >
-          <ActionButton
-            icon={
-              <MaterialIcons
-                name="group"
-                size={ACTION_BUTTON_ICON_SIZE}
-                color={ACTION_BUTTON_COLOR}
-              />
-            }
-            buttonSize={ACTION_BUTTON_SIZE}
-            onPress={() => {
-              navigation.navigate('MyGroup');
-            }}
-            title="グループ"
-          />
-        </View>
-      </View>
+        <ActionButtons />
+
+        <View></View>
+      </ScrollView>
     </View>
   );
 };
 
 const PROFILE_IMAGE_SIZE = 80;
-const ACTION_BUTTON_SIZE = 60;
-const ACTION_BUTTON_COLOR = '#666666';
-const ACTION_BUTTON_ICON_SIZE = 24;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  contentContainer: {
     alignItems: 'center',
     paddingTop: 10,
   },
@@ -171,15 +92,5 @@ const styles = StyleSheet.create({
   },
   age: {
     fontSize: 18,
-  },
-  actionButtons1: {
-    width: '60%',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    marginTop: 22,
-  },
-  editButton: {
-    // transform: [{ translateY: 18 }],
   },
 });
